5bac4610e8548799bf65cf3ed48fd828100ec9fb
rsincara@mail.ru <Maxonchiksixonenine619>
2022-10-20 00:22:22 +0500

    Added put method

--- server/routes/links.js
+++ server/routes/links.js
@@ -4,20 +4,29 @@ const db = require('../db/index');
 
 const router = express.Router();
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', function (req, res) {
     db('links').where({ id: req.params.id }).first().then((link) => {
-        console.log('LINK: ', link)
         res.json(link || {result: 'Not found'});
     });
 });
 
-router.post('', function (req, res, next) {
-    console.log(req.body)
+router.post('', function (req, res) {
     db('links').insert(req.body).then(() => {
         res.statusCode = 200;
         res.end(``);
     });
 });
 
+router.put('/:id', function (req, res) {
+    const linkBody = req.body;
+    const id = req.params.id;
+    db('links')
+        .where({id})
+        .update(linkBody)
+        .then(() => {
+            res.statusCode = 200;
+            res.end(``);
+        });
+});
 
 module.exports = router;
