96f5b1a8c17fe25aae88e82696e51024337a571e
rsincara@mail.ru <Maxonchiksixonenine619>
2022-11-03 00:38:24 +0500

    Done redis

--- loader/loader.js
+++ loader/loader.js
@@ -2,22 +2,36 @@ const fetch = require('node-fetch');
 const { createClient } = require('redis');
 const amqp = require('amqplib/callback_api');
 
-const client = createClient({
-    url: 'redis://redis:6379'
-});
-
-client.on('error', (err) => console.log('Redis Client Error', err));
-
-client.connect().then(() => {
-    console.log('Connected!');
-});
-
-
 const {
     user,
     pass,
     WEB_BASE_URL,
+    REDIS_NAME,
 } = process.env;
+
+const getStatusFromRedis = async (client, link) => {
+    return await client.get(link);
+}
+
+const updateLink = (id, status) => {
+    fetch(`http://${WEB_BASE_URL}/links/${id}`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'PUT',
+        body: JSON.stringify({
+            status,
+        })
+    });
+}
+
+const client = createClient({
+    url: `redis://${REDIS_NAME}:6379`
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+client.connect();
 
 amqp.connect(`amqp://${user}:${pass}@rmq:5672/%2F`, function (error0, connection) {
 
@@ -30,35 +44,25 @@ amqp.connect(`amqp://${user}:${pass}@rmq:5672/%2F`, function (error0, connection
         const queue = 'links';
 
         channel.assertQueue(queue, {
-
             durable: false,
-
         });
 
-        channel.consume(queue, function (msg) {
-            
+        channel.consume(queue, async function (msg) {
             const content = JSON.parse(msg.content.toString());
-            fetch(content.name)
-                .then((res) => {
-                    fetch(`http://${WEB_BASE_URL}/links/${content.id}`, {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        method: 'PUT',
-                        body: JSON.stringify({
-                            status: res.status
-                        })
+            const statusFromRedis = await getStatusFromRedis(client, content.name);
+
+            if (statusFromRedis) {
+                updateLink(content.id, statusFromRedis);
+            } else {
+                fetch(content.name)
+                    .then((res) => {
+                        updateLink(content.id, res.status);
+                        client.set(content.name, res.status);
                     });
-                });
-
+            }
             console.log(" [x] Received %s", msg.content.toString());
-
         }, {
-
             noAck: true
-
         });
-
     });
-
 });
